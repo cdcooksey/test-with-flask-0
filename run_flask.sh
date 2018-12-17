7c47b06455ab718ca04c4962f691ad2090b56eca
Charlie Cooksey <cdcooksey@gmail.com>
2018-12-17 15:25:59 -0700

    commits `./run_flask.sh` script and `get-pip.py`

--- run_flask.sh
+++ run_flask.sh
@@ -1,1 +1,5 @@
+python get-pip.py --user
+pip install flask --user
+pip install peewee --user
+echo "pip, flask, and peewee ORM installed. Running flask:"
 FLASK_APP=app.py FLASK_ENV=development flask run --host=0.0.0.0

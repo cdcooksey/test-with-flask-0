1aae008ab791500bd8a85b58211fa53c5cb46090
Charlie Cooksey <cdcooksey@gmail.com>
2018-12-17 20:17:22 -0700

    adds `EventsService` to `AppComponent`, makes API call

--- frontend/events-frontend/src/app/app.component.ts
+++ frontend/events-frontend/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventsService } from './events.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'DNC Events Challenge';
+
+  constructor(private eventsService: EventsService) { }
+
+	ngOnInit() {
+		return this.getEvents();
+	}
+	getEvents(): void {
+		this.eventsService.getEvents().subscribe();
+	}
 }

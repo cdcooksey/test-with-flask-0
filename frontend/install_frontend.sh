ad159a1c90f6aafb18bda87994897e149d86f9fb
Charlie Cooksey <cdcooksey@gmail.com>
2018-12-17 18:09:20 -0700

    uses binaries download to install NodeJS

--- frontend/install_frontend.sh
+++ frontend/install_frontend.sh
@@ -11,6 +11,10 @@
 
 mkdir node
 cd node/
-wget ihttps://nodejs.org/dist/v10.14.2/node-v10.14.2-linux-x64.tar.xz
+wget https://nodejs.org/dist/v10.14.2/node-v10.14.2-linux-x64.tar.xz
 tar xvf node-v10.14.2-linux-x64.tar.xz
-
+cd node-v10.14.2-linux-x64/bin/
+cd /home/vagrant/.local/bin/
+ln -s /vagrant/frontend/node/node-v10.14.2-linux-x64/bin/node node
+ln -s /vagrant/frontend/node/node-v10.14.2-linux-x64/bin/npm npm
+ln -s /vagrant/frontend/node/node-v10.14.2-linux-x64/bin/npx npx

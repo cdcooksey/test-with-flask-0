62683da491c2ddc0fb781f46a6b98fb767cd8804
Charlie Cooksey <cdcooksey@gmail.com>
2018-12-17 17:59:59 -0700

    abandons compiling from source. it takes 40 minutes on a fast machine

--- frontend/install_frontend.sh
+++ frontend/install_frontend.sh
@@ -1,5 +1,16 @@
+#sudo apt-get update
+#sudo apt-get install -y g++
+#mkdir node
+#cd node/
+#wget https://nodejs.org/dist/v10.14.2/node-v10.14.2.tar.gz
+#tar xvf node-v10.14.2.tar.gz
+#cd node-v10.14.2
+#./configure
+#make
+#sudo make install
+
 mkdir node
 cd node/
-wget https://nodejs.org/dist/v10.14.2/node-v10.14.2-linux-x64.tar.xz
-tar xf node-v10.14.2-linux-x64
+wget ihttps://nodejs.org/dist/v10.14.2/node-v10.14.2-linux-x64.tar.xz
+tar xvf node-v10.14.2-linux-x64.tar.xz
 

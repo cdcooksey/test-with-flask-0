2c69e66f1b0b203ff0bcfc581af665c0d7f02f58
Charlie Cooksey <cdcooksey@gmail.com>
2018-12-17 20:16:13 -0700

    adds events service

---
+++ frontend/events-frontend/src/app/events.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+	private baseUrl = 'http://127.0.0.1:5000/api/v1/';
+
+  constructor(private http: HttpClient) { }
+
+  getEvents(): Observable<any> {
+		return this.http.get(`${this.baseUrl}/events`);
+  }
+}

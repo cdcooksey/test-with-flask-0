71fea4d978444d20de37a5fea6cccbccd802083e
Charlie Cooksey <cdcooksey@gmail.com>
2018-12-17 18:56:13 -0700

    updates `install_frontend.sh`

--- frontend/install_frontend.sh
+++ frontend/install_frontend.sh
@@ -1,14 +1,3 @@
-#sudo apt-get update
-#sudo apt-get install -y g++
-#mkdir node
-#cd node/
-#wget https://nodejs.org/dist/v10.14.2/node-v10.14.2.tar.gz
-#tar xvf node-v10.14.2.tar.gz
-#cd node-v10.14.2
-#./configure
-#make
-#sudo make install
-
 # Install current version of Node / NPM
 mkdir node
 cd node/

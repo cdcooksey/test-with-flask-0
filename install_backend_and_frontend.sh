be76b99cb91094ab823ba1b9dc9803fbd1e20d90
Charlie Cooksey <cdcooksey@gmail.com>
2018-12-21 10:28:17 -0700

    adds `install_backend_and_frontend.sh`

---
+++ install_backend_and_frontend.sh
@@ -0,0 +1,13 @@
+cd frontend/events-frontend/
+./install_frontend.sh
+cd ../../backend/
+
+echo "----------------------------------------------"
+echo " The backend will run now automatically. Run  "
+echo " the frontend by opening another terminal and "
+echo " do:                                          "
+echo " cd frontend/events-frontend/                 "
+echo " ./run_angular.sh                             "
+echo "----------------------------------------------"
+
+./run_flask.sh

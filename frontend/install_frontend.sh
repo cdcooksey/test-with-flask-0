feca0ad6f490c04aae08aed407fea106f3aa97bb
Charlie Cooksey <cdcooksey@gmail.com>
2018-12-21 11:33:32 -0700

    adds sudo npm install -g @angular/cli

--- frontend/install_frontend.sh
+++ frontend/install_frontend.sh
@@ -15,4 +15,5 @@ npm install -g @angular/cli
 cd /home/vagrant/.local/bin/
 ln -s /vagrant/frontend/node/node-v10.14.2-linux-x64/lib/node_modules/@angular/cli/bin/ng ng
 cd /vagrant/frontend/events-frontend
+sudo npm install -g @angular/cli
 npm install

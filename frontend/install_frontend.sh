682e6dd440df7cfb92988d8ba9718303ceba35c3
Charlie Cooksey <ccooksey@bbasolutions.com>
2018-12-21 11:58:58 -0700

    updates `frontend/install_frontend.sh`

--- frontend/install_frontend.sh
+++ frontend/install_frontend.sh
@@ -3,17 +3,18 @@ mkdir node
 cd node/
 wget https://nodejs.org/dist/v10.14.2/node-v10.14.2-linux-x64.tar.xz
 tar xvf node-v10.14.2-linux-x64.tar.xz
-cd node-v10.14.2-linux-x64/bin/
-cd /home/vagrant/.local/bin/
-ln -s /vagrant/frontend/node/node-v10.14.2-linux-x64/bin/node node
-ln -s /vagrant/frontend/node/node-v10.14.2-linux-x64/bin/npm npm
-ln -s /vagrant/frontend/node/node-v10.14.2-linux-x64/bin/npx npx
+
+cd /usr/bin
+sudo ln -s /vagrant/frontend/node/node-v10.14.2-linux-x64/bin/node node
+sudo ln -s /vagrant/frontend/node/node-v10.14.2-linux-x64/bin/npm npm
+sudo ln -s /vagrant/frontend/node/node-v10.14.2-linux-x64/bin/npx npx
 
 # Install Angular
 cd /vagrant/frontend/
 npm install -g @angular/cli
-cd /home/vagrant/.local/bin/
-ln -s /vagrant/frontend/node/node-v10.14.2-linux-x64/lib/node_modules/@angular/cli/bin/ng ng
+
+# Install Angular CLI globally
+cd /usr/bin
+sudo ln -s /vagrant/frontend/node/node-v10.14.2-linux-x64/lib/node_modules/@angular/cli/bin/ng ng
 cd /vagrant/frontend/events-frontend
-sudo npm install -g @angular/cli
 npm install

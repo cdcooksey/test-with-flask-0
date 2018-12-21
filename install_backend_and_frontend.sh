273d8d4d6089bf6e699742a271a9a562691d3963
Charlie Cooksey <cdcooksey@gmail.com>
2018-12-21 11:04:52 -0700

    clean up

--- install_backend_and_frontend.sh
+++ install_backend_and_frontend.sh
@@ -8,6 +8,9 @@ echo " the frontend by opening another terminal and "
 echo " do:                                          "
 echo " cd frontend/events-frontend/                 "
 echo " ./run_angular.sh                             "
+echo "                                              "
+echo " After kicking off the frontend, open a web   "
+echo " browser and go to: http://0.0.0.0:4200/      "
 echo "----------------------------------------------"
 
 ./run_flask.sh
